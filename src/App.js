3eaf3637afdb5f9bd36a1106cd3c5e121bfc51fb
Jake O'Toole <48859911+SirPotatoIV@users.noreply.github.com>
2020-01-23 14:06:30 -0600

    Added paddle movement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import Paddle from "./components/Paddle";
 
 export default function App() {
+  const [playerOnePosition, setPlayerOnePosition] = useState(10);
+  const [playerTwoPosition, setPlayerTwoPosition] = useState(10);
+
+  function handleKeyPress(res) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/keypress_event
+    console.log(res.code);
+    switch (res.code) {
+      case "KeyW":
+        // code block
+        setPlayerOnePosition(playerOnePosition + 10);
+        break;
+      case "KeyS":
+        // code block
+        setPlayerOnePosition(playerOnePosition - 10);
+        break;
+      case "KeyO":
+        // code block
+        console.log("o case occurred");
+        setPlayerTwoPosition(playerTwoPosition + 10);
+        break;
+      case "KeyL":
+        // code block
+        setPlayerTwoPosition(playerTwoPosition - 10);
+        break;
+      default:
+        setPlayerOnePosition(0);
+        setPlayerTwoPosition(0);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keypress", handleKeyPress);
+    return () => window.removeEventListener("keypress", handleKeyPress);
+  }, [playerOnePosition, playerTwoPosition]);
+
   return (
-    <div className="container">
-      <Paddle />
-      <Paddle isPlayerTwo />
+    <div
+      className="container"
+      style={{
+        width: "400px",
+        height: "300px"
+      }}
+    >
+      <Paddle positionY={playerOnePosition} />
+      <Paddle positionY={playerTwoPosition} isPlayerTwo />
     </div>
   );
 }
--- src/components/Paddle.js
+++ src/components/Paddle.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Paddle.css";
 
-export default function Paddle({ isPlayerTwo }) {
+export default function Paddle({ isPlayerTwo, positionY }) {
+  console.log(positionY);
   return (
-    <div className={isPlayerTwo ? "paddle playerTwo" : "paddle playerOne"} />
+    <div
+      className={isPlayerTwo ? "paddle playerTwo" : "paddle playerOne"}
+      style={{
+        transform: `translateY(calc(-50% - ${positionY}px))`
+      }}
+    />
   );
 }
